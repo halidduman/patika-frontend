a91cc0181d0c47e22a1231e245826d6705a56adc
halidduman <halidduman60@gmail.com>
2023-04-01 17:21:22 +0300

    Koşul Yapısı Kullanımı (if / else)

---
+++ 1-baslangic/8-JavaScript/dersler/11-ders/kosul-yapisi-kullanimi.js
@@ -0,0 +1,13 @@
+////////////////////////////////// Koşul Yapısı Kullanımı (if / else)
+let username = prompt("kullanici adinizi giriniz");
+//eger kullanici bilgisi varsa ekrana yazdir
+
+// eger (user.length >0) {console.log.(username)} else console.log("bilgi yok")
+
+// if (user.length >0) {console.log.(username)}
+
+if (username.length > 0) { //if kismi her zaman true ise calisir
+  console.log(`Kullanici Bilginiz ${username}`);
+} else {
+  console.log("bilgi yok");
+}
