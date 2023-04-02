dedb692f9c56ec47df887e1304b1a2ba428334b9
halidduman <halidduman60@gmail.com>
2023-04-02 15:04:06 +0300

    ternary operator

---
+++ 1-baslangic/8-JavaScript/dersler/13-ders/ternary-operator.js
@@ -0,0 +1,8 @@
+///////////// Ternary Operatör ( Koşul ? Doğruysa : Yanlışsa) ile If Kullanımı
+
+// eger kullanici adin varsa yazdir yoksa yoksa kullanici bilginiz bulunmadi yaz
+
+let userName = prompt ("kullanici adinizi yaziniz")
+let info = document.querySelector("#info")
+
+info.innerHTML = `${userName.length > 0 ? userName : "kullanici biginiz bulunamadi"}`
