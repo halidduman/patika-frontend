d960c2fe2bc228fe939fe687e30733ab190d58e3
halidduman <halidduman60@gmail.com>
2023-04-04 16:51:58 +0300

    Fonksiyona-Parametre-ve-Geridonus-Eklemek

---
+++ 1-baslangic/8-JavaScript/dersler/16-ders/params-return.js
@@ -0,0 +1,36 @@
+//////////////////////////////////Fonsiyona Parametre (params) ve Geridönüş (return) Eklemek 
+
+let firstName = "lorem"
+
+function greetings(firstName="",lastName="") {
+
+    // console.log(`merhaba ${firstName ? firstName : ""}`)
+    console.log (`merhaba ${firstName}`)
+    console.log (`merhaba ${firstName} ${lastName}`)
+}
+
+console.log(firstName)//degisgen
+greetings("apple")// fonksiyona parametre gondermedik?
+let info = greetings("parametre")
+console.log(info)
+
+function greetings2(firstName,lastName) {
+
+    let info = `merhaba ${firstName} ${lastName}`
+    return info
+}
+
+let greetingsInfo = greetings2("ad", "soyad")
+
+console.log(greetingsInfo)
+
+function domIdWriteInfo (id, info) {
+
+    let domObject = document.querySelector(`#${id}`)
+    domObject.innerHTML = info
+
+}
+let htmlInfo = `<span style="color:red">color Redddd</span>`
+
+domIdWriteInfo("greeting", htmlInfo)
+domIdWriteInfo(`info`,greetings2 ("Lorem", "Ipsum"))
