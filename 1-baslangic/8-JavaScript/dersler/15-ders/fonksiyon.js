74f2a50637dde5cb22239c68c4f4197b74dc12d3
halidduman <halidduman60@gmail.com>
2023-04-04 15:04:46 +0300

    fonksiyon-nedir

---
+++ 1-baslangic/8-JavaScript/dersler/15-ders/fonksiyon.js
@@ -0,0 +1,31 @@
+// Fonksiyon Nedir? Neden Kullanırız?
+
+function hello () {
+
+    console.log("merhaba")
+    console.log("helloWorld")
+
+}
+
+function helloWorld () {
+
+    console.log("helloWorld")
+}
+
+
+///////////////////////////////////////////////// hata alabiliriz...
+// function userChack () {
+//     if (userName && age >= 18) {
+//         info.innerHTML = "ehliyet alabilir"
+//     }else if (!userName) {
+//         info.innerHTML = "kullaniciadiniz yok"
+//     } else if (!(age >= 18)) {
+//         info.innerHTML = "yas bilginiz yok veya 18 yasindan kucuksunuz!"
+//     }
+
+// }
+
+hello ()
+
+
+
