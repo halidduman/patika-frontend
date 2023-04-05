671786b693af3e4f88483a9abcffa938d795b549
halidduman <halidduman60@gmail.com>
2023-04-05 12:20:40 +0300

    fat-arrow

---
+++ 1-baslangic/8-JavaScript/dersler/17-ders/fat-arrow.js
@@ -0,0 +1,16 @@
+// Fat Arrow () => Fonksiyonu Kullanımı
+
+function hello(firstName) {
+  console.log(`merhaba ${firstName}`);
+}
+
+hello("JavaScript");
+
+const helloFuncV1 = (FirstName) => {
+  console.log(`merhaba ${firstName}`)
+}
+
+helloFuncV1("helloFuncV1");
+
+const helloFuncV2 = (firstName) => console.log(`merhaba ${firstName} ${lastName}`);
+helloFuncV1("helloFuncV2");
