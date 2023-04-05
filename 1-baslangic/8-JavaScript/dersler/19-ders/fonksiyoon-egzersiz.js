b19fb3a541091278f1e00162bd427239c12a59ff
halidduman <halidduman60@gmail.com>
2023-04-05 13:38:05 +0300

    Fonksiyonlar-Bolum-Sonu-Egzersizi

---
+++ 1-baslangic/8-JavaScript/dersler/19-ders/fonksiyoon-egzersiz.js
@@ -0,0 +1,41 @@
+// Fonksiyonlar - Bölüm Sonu Egzersizi
+
+let counter = 0;
+let counterDOM = document.querySelector("#counter");
+let increaseDOM = document.querySelector("#increase");
+let decreaseDOM = document.querySelector("#decrease");
+
+counterDOM.innerHTML = counter;
+
+increaseDOM.addEventListener("click", clickEvent);
+decreaseDOM.addEventListener("click", clickEvent);
+
+function clickEvent() {
+  console.log(this.id);
+
+  this.id == "increase" ? counter += 1: counter -= 1
+  counterDOM.innerHTML = counter    
+  }
+  
+
+/////////////////////////////////////////////////////ilk kod
+// function clickEvent() {
+//     console.log(this.id);
+  
+//     if (this.id == "increase") {
+//       counterDOM.innerHTML = counter += 1
+//     }else {
+//       counterDOM.innerHTML = counter -= 1
+//     }
+    
+//   }
+  
+/////////////////////////////////////////////////////ikinci kod
+// function clickEvent() {
+//     console.log(this.id);
+  
+//     this.id == "increase" ? counterDOM.innerHTML = counter += 1 :  counterDOM.innerHTML = counter -= 1
+     
+//     }
+    
+  
