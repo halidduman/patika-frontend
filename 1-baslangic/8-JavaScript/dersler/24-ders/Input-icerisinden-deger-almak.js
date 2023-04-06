07669b62a90ec0250533a8c04f3a30cf71d33059
halidduman <halidduman60@gmail.com>
2023-04-06 13:26:39 +0300

    Input-icerisinden-deger-almak

--- 1-baslangic/8-JavaScript/dersler/24-ders/Input-icerisinden-deger-almak.js
+++ 1-baslangic/8-JavaScript/dersler/24-ders/Input-icerisinden-deger-almak.js
@@ -1,9 +1,15 @@
-// Form ve Form Submit Yönetimi
+// Input içerisinden Değer Almak
 
 let formDOM = document.querySelector("#userForm");
 formDOM.addEventListener("submit", formSubmit);
 
+
 function formSubmit(event) {
   event.preventDefault(); //default islemi engelledk...
   console.log("islem gerceklesti");
+
+  let scoreInputDOM = document.querySelector("#score")
+  console.log(scoreInputDOM.value)
+  localStorage.setItem("score",scoreInputDOM.value )
 }
+
