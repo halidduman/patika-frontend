cf27eb63b6967e3010b549041b1e92e456e76b57
halidduman <halidduman60@gmail.com>
2023-04-08 13:54:54 +0300

    array-forEach-metotu

---
+++ 1-baslangic/8-JavaScript/dersler/29-ders/array-forEach-kullanimi.js
@@ -0,0 +1,26 @@
+/////////////////////////////// Array forEach Metotu
+
+const PRODUCTS = ["Laptop", "Phone", "Speaker", "Desktop PC", "Server", "Mouse", "Keyboard"]
+
+// PRODUCTS.forEach((product, index, array) => array[index] = product + " 111" )
+PRODUCTS.forEach((product, index, array) => array[index] = `${product.toUpperCase()}` )  // toUpperCase
+
+console.log(PRODUCTS)
+
+/*****
+const userListDOM = document.querySelector('#userList')
+
+for (index = 0; index < users.length; index++) {
+  const liDOM = document.createElement('li')
+  liDOM.innerHTML = users[index]
+  userListDOM.appendChild(liDOM)
+}
+*/
+
+const userListDOM = document.querySelector('#userList')
+
+PRODUCTS.forEach( item => {
+  const liDOM = document.createElement('li')
+  liDOM.innerHTML = item
+  userListDOM.append(liDOM)
+})
